c91a4599739fe7b136cb31b3b0439aad6c5e709f
Sandro Groganz <sandro@campaignchain.com>
2016-07-16 12:32:22 -0700

    CampaignChain/campaignchain#192 Subsume all app-wide parameters under campaignchain_core using tree builder

--- CampaignChainOperationGoToWebinarBundle.php
+++ CampaignChainOperationGoToWebinarBundle.php
@@ -2,8 +2,13 @@
 
 namespace CampaignChain\Operation\GoToWebinarBundle;
 
+use CampaignChain\Operation\GoToWebinarBundle\DependencyInjection\CampaignChainOperationGoToWebinarExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class CampaignChainOperationGoToWebinarBundle extends Bundle
 {
+    public function getContainerExtension()
+    {
+        return new CampaignChainOperationGoToWebinarExtension();
+    }
 }
--- DependencyInjection/CampaignChainOperationGoToWebinarExtension.php
+++ DependencyInjection/CampaignChainOperationGoToWebinarExtension.php
@@ -2,9 +2,9 @@
 
 namespace CampaignChain\Operation\GoToWebinarBundle\DependencyInjection;
 
+use CampaignChain\CoreBundle\Bundle\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
 /**
@@ -15,7 +15,7 @@ use Symfony\Component\DependencyInjection\Loader;
 class CampaignChainOperationGoToWebinarExtension extends Extension
 {
     /**
-     * {@inheritdoc}
+     * {@inheritDoc}
      */
     public function load(array $configs, ContainerBuilder $container)
     {
@@ -24,5 +24,7 @@ class CampaignChainOperationGoToWebinarExtension extends Extension
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+
+        $this->setParameters($container, $config);
     }
 }
